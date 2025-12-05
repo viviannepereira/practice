09cceaea1d2acd2996b400ef1c345b6babae1f8e
vivi <145087554+viviannepereira@users.noreply.github.com>
2025-12-05 14:39:06 +0000

    hi

--- ppm.py
+++ ppm.py
@@ -1,7 +1,14 @@
+import re
+
 class PPM:
     def __init__(self, input_file, output_file):
 
         self.list = self.convert_to_list(input_file)
+        self.output_file = output_file
+        self.magic_number = self.list[0][0][0]
+        self.columns = self.list[1][0][1]
+        self.rows = self.list[1][0][0]
+        self.max_colour_val = self.list[2][0][0]
 
     def convert_to_list(self, input_file):
 
@@ -13,22 +20,41 @@ class PPM:
 
         # split each pixel into a list
 
+        chunk_size = 3
+
+        index = 0
+
         for line in lines:
 
-            #number_of_groups = len(line)//3
-            for num in range(0,len(line)):
-                line[:num].split()
-                num += 3
-            
+            line = line.split()
 
+            new_list = []
+
+            for num in range(0, len(line), chunk_size):
+
+                new_list.append(line[num:num + chunk_size])
+
+            lines[index] = new_list
+
+            index += 1
 
 
         return lines
     
 
 def main():
-    my_ppm = PPM("cake.ppm", "new_cake.ppm")
-    print(my_ppm.list)
+    while True:
+
+        input_file = input("Please name the file you would like to manipulate: ")
+
+        match_object = re.search(r"^[\d\w-_]+\.ppm$", input_file)
+
+        if match_object is not None:
+            break
+        print("Incorrect file format, please try again.")
+
+    my_ppm = PPM("mini_squares.ppm", "new_squares.ppm")
+    
 
 main()
 
